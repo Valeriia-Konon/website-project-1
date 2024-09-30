4ecf21a83e12ebe0e461dde0c8f9987a496200da
Valeriia Konon <konon.valeriia@gmail.com>
2024-09-29 18:14:37 -0600

    Project completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ function Footer() {
               className="footer-input"
               placeholder="Your Email"
             />
-            <Button buttonStyle="btn-outline">Subscribe</Button>
+            <Button buttonStyle="btn--outline">Subscribe</Button>
           </form>
         </div>
       </section>
@@ -60,6 +60,58 @@ function Footer() {
           </div>
         </div>
       </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRVL ADVNTR <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">TRVL ADVNTR 2020</small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f"></i>
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram"></i>
+            </Link>
+            <Link
+              className="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i className="fab fa-youtube"></i>
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i className="fab fa-twitter"></i>
+            </Link>
+            <Link
+              className="social-icon-link linkedin"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <i className="fab fa-linkedin"></i>
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
